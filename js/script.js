f5dd3eb670e3ce3857c33921cdd5b238a1d042d2
João Pedro <Joaosilva8882@gmail.com>
2023-02-09 11:37:31 -0300

    feat: add javascript

--- js/script.js
+++ js/script.js
@@ -19,3 +19,68 @@ btnHamburguer.addEventListener("click", () => {
   navigation.classList.toggle('abrir')
   btnHamburguer.classList.toggle("abrir");
 });
+
+
+// /* Titulo pricipal */
+
+// const titlePrimary = document.querySelectorAll('.titulo-primary')
+
+
+// function handleEntrada( titulo ) {
+  
+// }
+
+// titlePrimary.forEach( handleEntrada )
+
+/* Sobre */
+
+const btnsSobre = document.querySelectorAll('.btn-time')
+
+function handleMudarTexto(foto) {
+  foto.addEventListener('click', handleTrocar)
+}
+
+function handleTrocar( event ) {
+  const ano = event.target
+  const hTitulo = document.querySelector('.historia-conteudo h2')
+  const hParag = document.querySelector('.historia-conteudo p')
+  const himg = document.querySelector('.historia-imagem img')
+
+  const getDados = async () => {
+    const response = await fetch(`../dados.json`)
+    const json = await response.json() 
+
+    const anoJson = json.filter(( anos ) => anos.ano === ano.innerHTML)
+
+    if( anoJson ) {
+      anoJson.map((item) => {
+        hTitulo.innerHTML = item.titulo
+        hParag.innerHTML = item.historia
+        himg.src = item.foto
+      })
+    }
+  }
+  getDados()
+}
+
+btnsSobre.forEach( handleMudarTexto )
+
+
+/* Portfólio */
+
+const fotosGaleria = document.querySelectorAll('.portfolio-galeria.galeria1 img')
+const fotoPrincipal = document.querySelector('#galeria1 img')
+
+function trocarPredio( event ) {
+  const foto = event.currentTarget
+  fotoPrincipal.setAttribute('src', foto.getAttribute('src'))
+}
+
+function handleTracarFotoGaleria (foto) {
+  foto.addEventListener('click', trocarPredio)
+}
+
+fotosGaleria.forEach( handleTracarFotoGaleria )
+
+// Termos
+
